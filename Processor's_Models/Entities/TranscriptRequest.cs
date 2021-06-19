aedf40571f302a56d990cbdd7c4c6dd32085dc3c
Goodebag <egbule.goodluck@gmail.com>
2021-06-19 17:40:25 +0100

    User added

---
+++ Processor's_Models/Entities/TranscriptRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Processor_s_Models.Entities
+{
+   public  class TranscriptRequest
+    {
+        [Key]
+        public long OrderNumber { get; set; }
+    }
+}
---
+++ Processor's_Models/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Processor_s_Models.Entities
+{
+   public class User
+    {
+        public long UserId { get; set; }
+        public string FirstName { get; set; }
+        public string  LastName { get; set; }
+        public string FullName { get { return LastName + " " + FirstName; } }
+
+    }
+}
