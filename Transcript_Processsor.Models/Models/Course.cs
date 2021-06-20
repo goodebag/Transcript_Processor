ef39f2d5079dd7ea4317f3b94c799c4d35afe2e0
Goodebag <egbule.goodluck@gmail.com>
2021-06-20 07:42:39 +0100

    Merge issues finally resolved

--- Transcript_Processsor.Models/Models/Course.cs
+++ Transcript_Processsor.Models/Models/Course.cs
@@ -9,12 +9,7 @@ namespace Transcript_Processsor.Models.Models
         public int CourseId { get; set; }
         public string CourseCode { get; set; }
         public string CourseName { get; set; }
-<<<<<<< HEAD:Transcript_Processsor.Models/Models/Course.cs
-        public short UnitLoad { get; set; }
-=======
         public byte UnitLoad { get; set; }
-
->>>>>>> f542b10ba58514ed430e3a0bc2d99eb2e46f9bd5:Processor's_Models/Models/Course.cs
         public int DeptId { get; set; }
         public virtual Department Dept { get; set; }
 
